4383a33ddb5326fb406592d2edec0c06b9ae8fc8
Leonel2099 <ezequielleonel1@gmail.com>
2023-10-01 15:31:26 -0300

    Se maqueto la interfaz de setting y levels . Tambien la logica

--- PrimerPrototitpo-DVJ-II/Assets/Basic/MainMenu/MenuCode.cs
+++ PrimerPrototitpo-DVJ-II/Assets/Basic/MainMenu/MenuCode.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MenuCode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    UIDocument mainMenu;
+    [SerializeField]
+    GameObject UiMenu, UiSettings, UiLevels;
+    public UIDocument settings;
+    Button btn_play;
+    Button btn_settings;
+    Button btn_levels;
+    private void OnEnable()
+    {        
+        mainMenu = GetComponent<UIDocument>();
+        VisualElement root = mainMenu.rootVisualElement;
+        btn_play = root.Q<Button>("play");
+        btn_play.RegisterCallback<ClickEvent,int>(SceneChange,1);
+        btn_settings = root.Q<Button>("settings");
+        btn_settings.RegisterCallback<ClickEvent>(UiChangeSettings);
+        btn_levels = root.Q<Button>("level");
+        btn_levels.RegisterCallback<ClickEvent>(UiChangeLevels);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UiChangeLevels(ClickEvent evt)
     {
-        
+        UiMenu.SetActive(false);
+        UiLevels.SetActive(true);
+    }
+
+    private void UiChangeSettings(ClickEvent evt)
+    {
+        UiMenu.SetActive(false);
+        UiSettings.SetActive(true);
+    }
+
+    private void SceneChange(ClickEvent evt,int scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 }
